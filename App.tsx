3d19bd1f79ff3855915d1d6e93c3b95328a5f681
Artasi67 <athurtavaressilva@gmail.com>
2025-02-12 08:49:40 -0300

    primeira alteração

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Tela_cadastro_produto from './src/views/Tela_cadastro_produto';
+
+export default function App() {
+  return (
+    <View>Hello World!</View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); 
---
+++ src/views/Tela_cadastro_produto.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text,TextInput, Button, Alert, StyleSheet } from "react-native";
+
+const Tela_cadastro_produto: React.FC = () => {
+
+    return(
+        <View>Olá Mundo!</View>
+    )
+}
+
+export default Tela_cadastro_produto;
