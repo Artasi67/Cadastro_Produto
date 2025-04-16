fe30275883917ac02c7a057e6b19f46e4189429a
Artasi67 <athurtavaressilva@gmail.com>
2025-04-16 08:45:00 -0300

    ta tudo dando errado

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Tela_cadastro_produto from './src/views/Tela_cadastro_produto';
 import Tela_listar_produtos from './src/views/tela_listar_produtos';
+import { NavigationContainer } from '@react-navigation/native';
+
+export type RootStackParamList = {
+  Cadastro: undefined,
+  Listagem: undefined
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <Tela_cadastro_produto/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Listagem'>
+        <Stack.Screen
+          name='Listagem'
+          component={Tela_listar_produtos}
+          options={{title: "Listagem de Produtos"}}
+        />
+        <Stack.Screen
+          name='Cadastro'
+          component={Tela_cadastro_produto}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
